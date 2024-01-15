6511ab55455f980d0cfc5d870b25102e41a119ca
JeongCheolLee <jclee7503@gmail.com>
2024-01-15 18:15:46 +0900

    fix: git ignore

--- envs/env.ts
+++
@@ -1,6 +0,0 @@
-let envFilePath;
-if (process.env.NODE_ENV === 'dev') envFilePath = 'envs/.env.dev';
-else if (process.env.NODE_ENV === 'prod') envFilePath = 'envs/.env.prod';
-else if (process.env.NODE_ENV === 'test') envFilePath = 'envs/.env.test';
-
-export default envFilePath;
